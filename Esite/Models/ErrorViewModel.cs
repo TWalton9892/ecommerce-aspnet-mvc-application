31173af7c41fd20aed6c6dbc4b20e02c074646ff
Trey Walton <twaltonsmiles@gmail.com>
2022-09-05 20:43:46 -0400

    Add project files.

---
+++ Esite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Esite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
